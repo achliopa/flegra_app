2e760678be08874212bc8ebab487c043c334e9fd
achliopa <athchliopanos@gmail.com>
2013-01-30 14:56:46 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlegraApp::Application.config.secret_token = '236861df43adaabc5c4ed18038de599a0926b51fabffaa680cbc8dd87d75c3cb0b471a35da6961f4715eeb4ef5ee0a87cb0fda180a73a95a44919d504b9e000f'
